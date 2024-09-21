2a75e8b5c032813c31523ad1c514fe5f58fd79a8
André Silva <andrefillype10@gmail.com>
2024-09-21 13:28:45 +0100

    [andre-silva]: userService implementation

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+
+@Injectable({
+    providedIn: 'root',
+})
+export class UserService {
+    private readonly inactivityDuration = 5000;
+    private inactivityTimer = this.createInactivityTimer();
+    private readonly inactivitySubject = new Subject<void>();
+    public readonly inactivity$ = this.inactivitySubject.asObservable();
+
+    constructor() {
+        window.addEventListener('click', this.resetActivityStatus.bind(this));
+    }
+
+    private resetActivityStatus(): void {
+        clearTimeout(this.inactivityTimer);
+        this.inactivityTimer = this.createInactivityTimer();
+    }
+
+    private createInactivityTimer(): any {
+        return setTimeout(() => {
+            this.inactivitySubject.next();
+        }, this.inactivityDuration);
+    }
+}
